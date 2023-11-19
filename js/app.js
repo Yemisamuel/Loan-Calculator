491a57cdaa13e88da408973b8dc8d08eb6093b0f
Yemisamuel <111006396+Yemisamuel@users.noreply.github.com>
2023-11-19 11:48:50 +0100

    Updated 19/11

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
-const form = document.getElementById('load-form');
-form.addEventListener('submit', function calculateLoan(e){
+const form = document.getElementById('loan-form');
+ form.addEventListener('submit', calculateLoan);
 
+ function calculateLoan(e){
+    e.preventDefault()
     const amount = document.getElementById('amount');
     const interest = document.getElementById('interest');
     const years = document.getElementById('years');
@@ -10,19 +12,53 @@ form.addEventListener('submit', function calculateLoan(e){
     const totalInterest = document.getElementById('total-interest');
 
 
-    const principal = parseFloat(amount.value)
+    const principal = parseFloat(amount.value);
     const calculatedInterest = parseFloat(interest.value) / 100 / 12;
     const calculatedPayment = parseFloat(years.value) * 12;
 
-    const x = math.pow(1 + calculatedInterest, calculatedPayment);
+    
+    let x = Math.pow(1 + calculatedInterest, calculatedPayment);
 
     const monthly = (principal *x* calculatedInterest);
 
     if(isFinite(monthly)){
         monthlyPayment.value = monthly.toFixed(2);
-        totalPayment.value = (monthly * calculatedPayment).toFixed(2);
-        totalInterest.value = ((monthly * calculatedPayment) -principal).toFixed(2);
+        totalPayment.value = ((monthly * calculatedPayment) -principal).toFixed(2);
+        totalInterest.value = (monthly * calculatedPayment).toFixed(2);
+
+        document.getElementById('result').style.display = 'block'
+        
+        document.getElementById('loader').style.display = 'none'
+        document.getElementById('amount').value = '';
+        document.getElementById('interest').value = '';
+        document.getElementById('years').value = '';
     }
 
-e.preventDefault();
-})
+    else{
+         
+        function showError(error){
+          document.getElementById('result').style.display = 'none'
+          document.getElementById('loader').style.display = 'none'
+          const errorDiv = document.createElement('div');
+  
+          const card = document.querySelector('.card')
+          const heading = document.querySelector('.heading')
+  
+          errorDiv.className = 'alert alert-danger';
+          errorDiv.appendChild(document.createTextNode(error));
+  
+          card.insertBefore(errorDiv, heading);
+  
+          setTimeout(clearErr, 3000)
+  
+        }
+        
+        showError('fill the the input');
+        function clearErr(){
+          document.querySelector('.alert').remove()
+        }
+      }
+ }
+   
+
+ 
